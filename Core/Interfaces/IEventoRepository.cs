0b42f96136bf7bed7823dc012d9578908ccb85a0
DouglasKenjiSakakibara01 <douglaskenjisakakibara@gmail.com>
2025-08-26 22:37:21 -0300

    ajustes no gitignore

---
+++ Core/Interfaces/IEventoRepository.cs
@@ -0,0 +1,14 @@
+﻿using NoteTakingAPI.Core.Entities;
+
+namespace NoteTakingAPI.Core.Interfaces
+{
+    public interface IEventoRepository
+    {
+        Task<Evento?> GetByIdAsync(int id);
+        Task<IEnumerable<Evento>> GetAllAsync();
+        Task<Evento> InserOrUpdateAsync(Evento evento);
+        Task DeleteAsync(int id);
+        Task<bool> SaveChangesAsync();
+
+    }
+}
